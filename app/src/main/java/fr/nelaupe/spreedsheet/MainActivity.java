0f10d87d104b9709be2479501c51c8328d38a9d3
Lucas Nelaupe <lucas34990@gmail.com>
2015-10-12 13:22:27 +0200

    Split sample

--- app/src/main/java/fr/nelaupe/spreedsheet/MainActivity.java
+++ app/src/main/java/fr/nelaupe/spreedsheet/MainActivity.java
@@ -1,17 +1,10 @@
 package fr.nelaupe.spreedsheet;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.TextView;
 
 import org.fluttercode.datafactory.impl.DataFactory;
 
-import fr.nelaupe.spreadsheetlib.SimpleTextAdaptor;
-import fr.nelaupe.spreadsheetlib.SpreadSheetCell;
 import fr.nelaupe.spreadsheetlib.SpreadSheetView;
 
 public class MainActivity extends Activity {
@@ -26,30 +19,17 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         SpreadSheetView spreadSheetView = (SpreadSheetView) findViewById(R.id.spread_sheet);
-
         initSpreadSheet(spreadSheetView);
-//        initSpreadSheetWithAdaptor(spreadSheetView);
-
         spreadSheetView.invalidate();
     }
 
-    // Simple but not flexible. Create only text row.
-    private void initSpreadSheet(SpreadSheetView spreadSheetView) {
+    protected void initSpreadSheet(SpreadSheetView spreadSheetView) {
         for (int i = 0; i < 30; i++) {
             spreadSheetView.getAdaptor().add(generateDummyData(i));
         }
     }
 
-    private void initSpreadSheetWithAdaptor(SpreadSheetView spreadSheetView) {
-        CustomCellAdaptor cellAdaptor = new CustomCellAdaptor(this);
-        for (int i = 0; i < 30; i++) {
-            cellAdaptor.add(generateDummyData(i));
-        }
-        spreadSheetView.setAdaptor(cellAdaptor);
-    }
-
-
-    private Data generateDummyData(int i) {
+    protected Data generateDummyData(int i) {
         DataFactory df = new DataFactory();
 
         Data data = new Data();
@@ -65,39 +45,6 @@ public class MainActivity extends Activity {
         return data;
     }
 
-    private View inflateTextView(String text) {
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.cell_textview, null, false);
-
-        TextView textView = (TextView) view.findViewById(R.id.text);
-        textView.setText(text);
-
-        return view;
-    }
-
-    private View inflateCheckbox(Boolean bool) {
-        CheckBox checkBox = new CheckBox(MainActivity.this);
-        checkBox.setChecked(bool);
-        checkBox.setEnabled(false);
-        return checkBox;
-    }
-
-    private class CustomCellAdaptor extends SimpleTextAdaptor {
-
-        public CustomCellAdaptor(Context context) {
-            super(context);
-        }
-
-        @Override
-        public View getCellView(SpreadSheetCell cell, Object object) {
-            if (object.getClass().equals(Boolean.class)) {
-                return inflateCheckbox((Boolean) object);
-            } else {
-                return inflateTextView(object.toString());
-            }
-        }
-    }
-
     @Override
     protected void onResume() {
         super.onResume();
@@ -105,4 +52,5 @@ public class MainActivity extends Activity {
         long diff = System.currentTimeMillis() - start;
         System.out.println("TIME TO LOAD : "+diff);
     }
+
 }
