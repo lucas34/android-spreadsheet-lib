d3081c33d1fa4c4763a3853e4ae70c579a8f0891
Lucas Nelaupe <lucas.nelaupe@vcube.co.jp>
2016-02-16 16:27:22 +0800

    Put field binder abstract

--- spreadsheet/src/main/java/fr/nelaupe/spreadsheetlib/BinderField.java
+++ spreadsheet/src/main/java/fr/nelaupe/spreadsheetlib/BinderField.java
@@ -3,12 +3,10 @@ package fr.nelaupe.spreadsheetlib;
 /**
  * Created by lucas34990 on 16/2/16.
  */
-public class BinderField<TSelf extends SpreadSheetData> {
+public abstract class BinderField<TSelf extends SpreadSheetData> {
 
     public java.util.List<fr.nelaupe.spreadsheetlib.AnnotationFields> fields;
 
-    public Object getValueAt(String fieldName, TSelf data) {
-        return null;
-    }
+    public abstract Object getValueAt(String fieldName, TSelf data);
 
 }
